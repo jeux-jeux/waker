9bdc02231d0a3732159d95cec5cc07ecfede935a
Daniel Ersen <201269086+jeux-jeux@users.noreply.github.com>
2025-11-02 19:38:23 +0100

    Remove logging configuration from waker.py
    Removed logging setup from the Flask app.

--- waker.py
+++ waker.py
@@ -3,21 +3,13 @@ from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 import requests
-import logging
-
-
 
 app = Flask(__name__)
-app.logger.setLevel(logging.DEBUG)
 
 CLE = os.environ.get('CLE_ULTRA')
 URL = os.environ.get('URL')
-
-
-
 resp = requests.post(URL, json={"cle": CLE}, timeout=5 )
 resp.raise_for_status()
 j = resp.json()
 wake = j.get("awake_url")
-
 resp = requests.post(wake, json={"cle": CLE}, timeout=5 )
