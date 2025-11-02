98da0e640a17c05b34335ddc8c5e32f1a254da94
Daniel Ersen <201269086+jeux-jeux@users.noreply.github.com>
2025-11-02 18:30:35 +0100

    Add Flask app with environment variable support

--- waker.py
+++ waker.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+import requests
 
+
+
+app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
+
+CLE = os.environ.get('CLE')
+URL = os.environ.get('URL')
+
+
+
+resp = requests.post(URL, json={"cle": CLE}, timeout=5 )
+resp.raise_for_status()
+j = resp.json()
+wake = j.get("awake_url")
+
+resp = requests.post(wake, json={"cle": CLE}, timeout=5 )
