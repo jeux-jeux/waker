75d9f441192aefe417a9a4d71925701b2e2f4151
Daniel Ersen <201269086+jeux-jeux@users.noreply.github.com>
2025-11-02 16:17:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,26 @@ app.logger.setLevel(logging.DEBUG)
 CLE = os.environ.get('CLE')
 URL = os.environ.get('URL')
 
-resp = requests.post(f"{URL}cle-ultra", json={"cle": cle_received}, timeout=5 )
+data_cache = {
+    "manager":0, 
+    "cloudlink":0, 
+    "message":0, 
+    "firebase":0
+}
+
+resp = requests.post(URL, json={"cle": CLE}, timeout=5 )
 resp.raise_for_status()
 j = resp.json()
 awake = j.get("awake")
 
 def check_health(proxy):
     if awake[proxy] == "yes":
-        
-
+        url = j[proxy + "_url"]
+        resp = requests.post(url, json={"cle": CLE}, timeout=4 )
+            if not j["status"] or j["status"] == "ok":
+                if data_cache[proxy] < 3:
+                    resp = requests.post(j["ntfy_url"], json={"cle": CLE}, timeout=5 )                    
+                
 @app.route("/", methods=["POST"])
 def wake():
     data = request.get_json(force=True, silent=True) or {}
@@ -28,8 +39,8 @@ def wake():
     if cle_received:
         resp = requests.post(f"{URL}cle-ultra", json={"cle": cle_received}, timeout=5 )
         resp.raise_for_status()
-        j = resp.json()
-        access = j.get("access")
+        x = resp.json()
+        access = x.get("access")
     if access == "false" or not cle_received:
         return jsonify({"status": "error", "message": "clé invalide"})
 
